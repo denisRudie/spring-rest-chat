9974e2ed09a625cabeea4e2e24ea9211daa14ec2
denisRudie <moonstamp1964@gmail.com>
2021-01-13 13:52:44 +0300

    first commit

---
+++ src/main/java/ru/job4j/springrestchat/SpringRestChatApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.springrestchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/job4j/springrestchat/SpringRestChatApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.springrestchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
